e3d6e46f2a2dcca935af970f4b0a35330bb6df82
Marufur Rahman <41162942+MarufurRahmanShovon@users.noreply.github.com>
2021-12-14 12:56:25 +0600

    Add files via upload

---
+++ client/src/Data.js
@@ -0,0 +1,102 @@
+export const SliderItems = [
+  {
+    id: 1,
+    img: "https://st3.depositphotos.com/13194036/i/600/depositphotos_361423028-stock-photo-fashionable-young-couple-summer-clothes.jpg",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd"
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?cs=srgb&dl=pexels-chloe-1043474.jpg&fm=jpg",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://sc01.alicdn.com/kf/U0443689dd6214b81b97f675b49393ed5B.jpg",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  }
+]
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://www.grameenuniqlo.com/pub/media/catalog/product/cache/image/900x900/e9c3970ab036de70892d86c6d221abfe/e/i/eid_2nd_period_website_tops-05.jpg",
+    title: "PANJABI",
+    category: "PANJABI",
+  },
+  {
+    id: 2,
+    img: "https://cdn.shopify.com/s/files/1/0752/6435/products/IMG_0005_2.jpg?v=1634377223",
+    title: "SHIRTS",
+    category: "SHIRTS",
+  },
+  {
+    id: 3,
+    img: "https://sc01.alicdn.com/kf/U0443689dd6214b81b97f675b49393ed5B.jpg",
+    title: "T-SHIRTS",
+    category: "T-SHIRTS",
+  },
+
+]
+export const categoriesw = [
+  {
+    id: 1,
+    img: "https://i.pinimg.com/originals/2f/d2/30/2fd2307c4e3303e64675b573c19477ad.jpg",
+    title: "SAREE",
+    category: "SAREE",
+  },
+  {
+    id: 2,
+    img: "https://looksgud.com/blog/wp-content/uploads/2017/08/1.straight-cut-salwar-suit.jpg",
+    title: "SHALWAR KAMEEZ",
+    category: "SHALWAR KAMEEZ",
+  },
+  {
+    id: 3,
+    img: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/12159736/2020/9/9/7913b945-c35a-454c-b792-e0a85c94c4a31599627286252-WEAVERS-VILLA-Women-Red-Solid-Shawl-2191599627285616-1.jpg",
+    title: "SHAWLS",
+    category: "SHAWLS",
+  },
+
+]
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+  },
+  {
+    id: 2,
+    img: "https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+  },
+  {
+    id: 3,
+    img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+  },
+  {
+    id: 4,
+    img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+  },
+  {
+    id: 5,
+    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id: 6,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+  },
+  // {
+  //   id:7,
+  //   img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+  // },
+  {
+    id: 7,
+    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+  },
+
+]
